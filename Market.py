8f362d5e93af554f4b65a10d13c8f4efa3537a62
Kainixy <dump.ariadna@gmail.com>
2026-04-24 19:06:04 +0300

    Add tests and CI workflow

--- Market.py
+++ Market.py
@@ -1,14 +1,7 @@
 #######################################################
 class Thing:
-    def __init__(self
-                 , id: int
-                 , name: str = 'Banana'
-                 , description: str = 'Very nice banana!'
-                 , cost: float = 10
-                 , mass: int = 1
-                 , status: int = 1
-                 , documentation: str = 'banana_documentation.txt'):
-        self.__id = id
+    def __init__(self, id: int, name: str = 'Banana', description: str = 'Very nice banana!', cost: float = 10, mass: int = 1, status: int = 1, documentation: str = 'banana_documentation.txt'):
+        self._id = id
         self._cost = cost
         self._mass = mass
         self._status = status
@@ -18,7 +11,7 @@ class Thing:
 
     @property
     def id(self):
-        return self.__id
+        return self._id
 
     @property
     def cost(self):
@@ -50,11 +43,11 @@ class Thing:
                 raise ValueError(f'Status can be 0 or 1, got {status}!')
         self._status = status
 
-    def get_thing_status(self):
-        if self.status == 1:
-            return Message.send_ok(f'Status of thing {self.name} is active!')
-        elif self.status == 0:
-            return Message.send_ok(f'Status of thing {self.name} is not active!')
+    # def get_thing_status(self):
+    #     if self.status == 1:
+    #         return Message.send_ok(f'Status of thing {self.name} is active!')
+    #     elif self.status == 0:
+    #         return Message.send_ok(f'Status of thing {self.name} is not active!')
 
     def get_text_from_documentation(self):
         with open(self.documentation, 'r') as file:
@@ -73,62 +66,82 @@ class Thing:
 
 class Basket:
     def __init__(self, user_id:int):
-        self.__user_id = user_id
+        self._user_id = user_id
         self.items = list()
-        Message.send_ok(f'Basket for user with id {self.__user_id} created!')
+        Message.send_ok(f'Basket for user with id {self._user_id} created!')
 
     def __str__(self):
-        return f'Basket for user with id {self.__user_id} has {len(self.items)} items!'
+        return f'Basket for user with id {self._user_id} has {len(self.items)} items!'
 
     def add_to_basket(self, thing:Thing):
         self.items.append(thing)
-        return Message.send_ok(f'Thing with id {thing.id} added to basket of user with id {self.__user_id}!')
+        return Message.send_ok(f'Thing with id {thing.id} added to basket of user with id {self._user_id}!')
 
     def del_from_basket(self, thing:Thing):
         self.items = [item for item in self.items if item != thing]
-        return Message.send_ok(f'Thing with id {thing.id} deleted from basket of user with id {self.__user_id} created!')
+        return Message.send_ok(f'Thing with id {thing.id} deleted from basket of user with id {self._user_id} created!')
 
 class User:
     def __init__(self, id:int, name:str, age:int):
-        self.__id = id
+        self._id = id
         self.name = name
-        self.age = age
-        self.basket = Basket(self.__id)
-        self.balance = Balance(self.__id)
-        Message.send_ok(f'User with id {self.__id} created!')
+        self._age = age
+        self.basket = Basket(self._id)
+        self.balance = Balance(self._id)
+        Message.send_ok(f'User with id {self._id} created!')
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, age):
+        if age < 0:
+            raise ValueError(f'Age cannot be negative, got {age}')
+        self._age = age
 
     def __str__(self):
-        return f'id:{self.__id} name:{self.name} age:{self.age} balance:{self.balance.balance}'
-
-    def get_user_id(self):
-        print( f'User with id {self.__id} found!')
-        return self.__id
+        return f'id:{self._id} name:{self.name} age:{self.age} balance:{self.balance.balance}'
 
 class Balance:
     def __init__(self, user_id:int):
-        self.user_id = user_id
-        self.balance = 0
-        Message.send_ok(f'Balance for user with id {self.user_id} created!')
+        self._user_id = user_id
+        self._balance = 0
+        Message.send_ok(f'Balance for user with id {self._user_id} created!')
+
+    @property
+    def balance(self):
+        return self._balance
+
+    @balance.setter
+    def balance(self, balance):
+        if balance < 0:
+            raise ValueError(f'Balance cannot be negative, got {balance}')
+        self._balance = balance
 
     def __str__(self):
-        return f'User with id {self.user_id} balance:{self.balance}'
+        return f'User with id {self._user_id} balance:{self._balance}'
 
     def add_money(self, money:int):
         self.balance += money
-        return Message.send_ok(f'Balance updated on +{money}! Your balance now is {self.balance}!')
+        return Message.send_ok(f'Balance updated on +{money}! Your balance now is {self._balance}!')
 
     def take_money(self, money:int):
-        if money <= self.balance:
-            self.balance -= money
-            return Message.send_ok(f'Balance updated on -{money}! Your balance now is {self.balance}!')
+        if money <= self._balance:
+            self._balance -= money
+            return Message.send_ok(f'Balance updated on -{money}! Your balance now is {self._balance}!')
         else:
-            return Message.send_error(f'Error! Your balance is less than {money}!')
+            raise ValueError(f'Error! Your balance is less than {money}!')
 
 class Message:
-    @staticmethod
-    def send_error(text):
-        print(text)
-        return 1
+    # @staticmethod
+    # def send_error(text):
+    #     print(text)
+    #     return 1
 
     @staticmethod
     def send_ok(text):
---
+++ Tests/test_example.py
@@ -0,0 +1,51 @@
+﻿import pytest
+import sys
+import os
+
+# обавляем текущую папку в пути импорта
+sys.path.insert(0, os.getcwd())
+
+# Теперь импорт должен работать
+from Market import Balance, User, Thing, Basket
+
+@pytest.fixture
+def create_test_balance():
+    # справлено: Balance принимает user_id, а не баланс
+    test_balance = Balance(user_id=1)
+    test_balance.balance = 100  # устанавливаем начальный баланс
+    print(f'test balance created, info:{str(test_balance)}')
+    return test_balance
+
+def test_class_balance(create_test_balance):
+    assert create_test_balance.add_money(100) == 0
+    assert create_test_balance.take_money(100) == 0
+
+@pytest.fixture
+def create_test_user():
+    test_user = User(
+        id=1,
+        name="Steve",
+        age=21)
+    print(test_user)
+    return test_user
+
+def test_class_user(create_test_user):
+    assert create_test_user.id == 1
+
+@pytest.fixture
+def create_test_thing():
+    test_thing = Thing(1, 'test_thing', 'very nice thing to test', 100, 300, 1)
+    with open('test_thing.txt', 'w') as f:
+        f.write('Yo, this is test file!')
+    test_thing.documentation = f.name
+    yield test_thing
+    os.remove('test_thing.txt')
+
+@pytest.fixture
+def create_test_basket(create_test_user):
+    test_basket = Basket(create_test_user.id)
+    print(f'test basket created, info:{str(test_basket)}')
+    return test_basket
+
+def test_class_basket(create_test_basket, create_test_thing):
+    assert create_test_basket.add_to_basket(create_test_thing) == 0
